a715b1b3aa29ca46b6e46eb743dd3292a9c9f2ff
gcoderx <56091123+gcoderx@users.noreply.github.com>
2020-10-28 00:38:54 +0530

    simpler code

--- bucket_sort.c
+++ bucket_sort.c
@@ -1,73 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-struct node{
-    float num;
-    struct node *next;
-};
-
-void swap(struct node *a, struct node *b) 
+#include <algorithm> 
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+  
+// Function to sort arr[] of size n using bucket sort 
+void bucketSort(float arr[], int n) 
 { 
-    float temp = a->num; 
-    a->num = b->num; 
-    b->num = temp; 
-}
-
-void sort(struct node *start) 
-{ 
-    int swapped; 
-    struct node *ptr1; 
-    struct node *lptr = NULL; 
-    /* Checking for empty list */
-    if (start == NULL) 
-        return; 
-    do
-    { 
-        swapped = 0; 
-        ptr1 = start; 
-        while (ptr1->next != lptr){ 
-            if (ptr1->num > ptr1->next->num){  
-                swap(ptr1, ptr1->next); 
-                swapped = 1; 
-            } 
-            ptr1 = ptr1->next; 
-        } 
-        lptr = ptr1; 
+    // 1) Create n empty buckets 
+    vector<float> b[n]; 
+  
+    // 2) Put array elements in different buckets 
+    for (int i = 0; i < n; i++) { 
+        int bi = n * arr[i]; // Index in bucket 
+        b[bi].push_back(arr[i]); 
     } 
-    while (swapped); 
+  
+    // 3) Sort individual buckets 
+    for (int i = 0; i < n; i++) 
+        sort(b[i].begin(), b[i].end()); 
+  
+    // 4) Concatenate all buckets into arr[] 
+    int index = 0; 
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < b[i].size(); j++) 
+            arr[index++] = b[i][j]; 
 } 
-
-struct node * insert(struct node* root, float num){
-    struct node* freshnode = (struct node* )malloc(sizeof(struct node));
-    freshnode->num = num;
-    freshnode->next = root;
-    root = freshnode;
-    return root;
-}
-
-void bucket_sort(float arr[], int n){
-    struct node *buck[10] = {NULL};
-    for(int i = 0; i < n; i++){
-        int ind = arr[i] * 10; 
-        buck[ind] = insert(buck[ind], arr[i]);
-    }
-    for(int i = 0; i < 10; i++){
-        sort(buck[i]);
-    }
-    for(int i = 0; i < 10; i++){
-        struct node* temp = buck[i];
-        while(temp){
-            printf("%f  ", temp->num);
-            temp = temp->next;
-        }
-        printf("\n");
-    }
-}
-
-int main(int argc, char const *argv[])
-{
-    float arr[15] = {0.12, 0.02, 0.24, 0.91, 0.32, 0.41, 0.22, 0.50, 0.59, 0.72, 0.21, 0.25, 0.205, 0.001, 0.05};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bucket_sort(arr, n);
-    return 0;
-}
+  
+/* Driver program to test above function */
+int main() 
+{ 
+    float arr[] = { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    bucketSort(arr, n); 
+  
+    cout << "Sorted array is \n"; 
+    for (int i = 0; i < n; i++) 
+        cout << arr[i] << " "; 
+    return 0; 
+} 
